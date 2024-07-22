b3988f10bf149413a53b578ab1630dfd112b0c47
giarreh <giarreh1@gmail.com>
2024-07-23 00:00:08 +0200

    Added post mapping for games

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -3,6 +3,8 @@ using GameStore.api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
   new GameDto(1, "Super Mario Bros", "Platformer", 29.99m, new DateOnly(1985, 9, 13)),
   new GameDto(2, "The Legend of Zelda", "Action-adventure", 49.99m, new DateOnly(1986, 2, 21)),
@@ -20,7 +22,24 @@ List<GameDto> games = [
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+.WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("games", (CreateGameDto newGame ) => 
+{
+  GameDto game = new(
+    games.Count + 1,
+    newGame.Name,
+    newGame.Genre,
+    newGame.Price,
+    newGame.ReleaseDate
+  );
+
+  games.Add(game);
+
+  return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id}, game);
+});
 
 app.MapGet("/", () => "Hello World!");
 app.Run();
